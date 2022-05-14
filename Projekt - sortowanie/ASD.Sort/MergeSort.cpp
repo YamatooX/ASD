1e18a4a6406b60ef90427561141f87812959dd41
YamatooX <jarbatorek@gmail.com>
2022-05-14 21:26:01 +0200

    Mergesort

---
+++ Projekt - sortowanie/ASD.Sort/MergeSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    clock_t start = clock();
+
+    int array[100000];
+
+    ifstream file;
+    file.open("plik.txt", ios::in);
+
+    if (!file.good())
+    {
+        return 0;
+    }
+    else
+    {
+
+        for (int i = 0; i < 100000; i++)
+            file >> array[i];
+    }
+
+    file.close();
+
+    
+
+
+
+    clock_t end = clock();
+
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Czas: " << elapsed << "s";
+
+    return 0;
+}
