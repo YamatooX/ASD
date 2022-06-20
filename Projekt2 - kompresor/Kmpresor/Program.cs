16ae978c3d29397df465adf278aba5c0427464a2
YamatooX <jarbatorek@gmail.com>
2022-06-20 15:52:43 +0200

    final

---
+++ Projekt2 - kompresor/Kmpresor/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kmpresor
+{
+    class Program
+    {
+        static void heapify(ArrayList list, int size, int iterator)
+        {
+            KeyValuePair<char, int> iteratorPair = (KeyValuePair<char, int>)list[iterator];
+
+            int largest = iterator;
+            KeyValuePair<char, int> largestPair = (KeyValuePair<char, int>)list[largest];
+            int largestValue = largestPair.Value;
+
+            int leftChild = 2 * iterator + 1;
+            KeyValuePair<char, int> leftChildPair = (KeyValuePair<char, int>)list[leftChild];
+            int leftChildValue = leftChildPair.Value;
+
+            int rightChild = 2 * iterator + 2;
+            KeyValuePair<char, int> rightChildPair = (KeyValuePair<char, int>)list[rightChild];
+            int rightChildValue = rightChildPair.Value;
+
+            if (leftChild < size && leftChildValue > largestValue)
+                largest = leftChild;
+
+            if (rightChild < size && rightChildValue > largestValue)
+                largest = rightChild;
+
+            if (!iteratorPair.Equals(largestPair))
+            {
+                (largestPair, iteratorPair) = (iteratorPair, largestPair);
+                heapify(list, size, largest);
+            }
+        }
+
+
+        static void CountCharacters(char[] charArray, int[] intArray, string text)
+        {
+            for (int j = 32; j < 127; j++)
+            {
+                int counter = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if ((char)j != text[i])
+                        charArray[j] = (char)j;
+                    if ((char)j == text[i])
+                        counter++;
+                }
+                intArray[j] = counter;
+                //Console.WriteLine($"{charArray[j]}: {intArray[j]}");
+            }
+        }
+
+        static void heapifyList(ArrayList list, int size)
+        {
+            for (int i = (size) / 2 - 1; i >= 0; i--)
+                heapify(list, size, i);
+        }
+        static string ReadFromFileToString(string str)
+        {
+            string newString = "";
+            Stream s = new FileStream(@"D:\Studia\Zajęcia\Algorytmy i struktury danych\ćw\ASD\Projekt2 - kompresor\Kmpresor\plot.txt", FileMode.Open);
+            int val = 0;
+
+            while (true)
+            {
+                val = s.ReadByte();
+
+                if (val < 0)
+                    break;
+                newString += (char)val;
+            }
+
+            return newString;
+        }
+
+        static void EnqueArraysWithAnyValues(ArrayList queue, char[] charArr, int[] intArr)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            for (int i = 0; i < 127; i++)
+            {
+                if (intArr[i] != 0)
+                {
+                    dictionary.Add(charArr[i], intArr[i]);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> c in dictionary)
+            {
+                queue.Add(c);
+                //Console.WriteLine(c);
+            }
+        }
+        static string QueueToString(ArrayList queue)
+        {
+            string s = "";
+            foreach (var item in queue)
+            {
+                s += item.ToString();
+            }
+
+            //Console.WriteLine(s);
+            return s;
+        }
+
+        static void PutIntoFile(ArrayList queue)
+        {
+            StreamWriter sw = new StreamWriter(@"D:\Studia\Zajęcia\Algorytmy i struktury danych\ćw\ASD\Projekt2 - kompresor\Kmpresor\compessedFile.txt");
+
+            sw.WriteLine(QueueToString(queue));
+
+            Console.WriteLine("Done");
+            sw.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            var queue = new ArrayList();
+            string stringToCompress = "";
+            string arr = ReadFromFileToString(stringToCompress);
+            
+            char[] charArray = new char[127];
+            int[] intArray = new int[127];
+
+            CountCharacters(charArray, intArray, arr);
+
+            EnqueArraysWithAnyValues(queue, charArray, intArray);
+
+            PutIntoFile(queue);
+
+            heapifyList(queue, queue.Count-1);
+
+            for (int i = 0; i < queue.Count; i++)
+                Console.WriteLine(queue[i]);
+        }
+    }
+}
---
+++ Projekt2 - kompresor/Kompresor/HoffmanCompresor/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using HoffmanCompresor;
+using System.IO;
+
+namespace HuffmanTest
+{
+    class Program
+    {
+
+        static void heapify(int[] arr, char[] array, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                (arr[i], arr[largest]) = (arr[largest], arr[i]);
+                (array[i], array[largest]) = (array[largest], array[i]);
+                heapify(arr, array, n, largest);
+            }
+        }
+
+        static string ReadFromFileToString(string str)
+        {
+            string newString = "";
+            Stream s = new FileStream(@"D:\Studia\Zajęcia\Algorytmy i struktury danych\ćw\ASD\Projekt2 - kompresor\Kmpresor\plot.txt", FileMode.Open);
+            int val = 0;
+
+            while (true)
+            {
+                val = s.ReadByte();
+
+                if (val < 0)
+                    break;
+                newString += (char)val;
+            }
+
+            return newString;
+        }
+
+        static Dictionary<char, byte> MakeDictionary(string text)
+        {
+            char[] charArray = new char[127];
+            int[] intArray = new int[127];
+
+            for (int j = 32; j < 127; j++)
+            {
+                int counter = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if ((char)j != text[i])
+                        charArray[j] = (char)j;
+                    if ((char)j == text[i])
+                        counter++;
+                }
+                intArray[j] = counter;
+            }
+
+            for (int i = 0; i < intArray.Length; i++)
+                heapify(intArray, charArray, intArray.Length, i);
+
+            byte[] byteArr = new byte[charArray.Length];
+
+            for (int i = 0; i < byteArr.Length; i++)
+            {
+                byteArr[i] = (byte)i;
+            }
+
+                Dictionary<char, byte> dictionary = new Dictionary<char, byte>();
+            for (int i = 0; i < 127; i++)
+            {
+                if (intArray[i] != 0)
+                {
+                    dictionary.Add(charArray[i], byteArr[i]);
+                }
+            }
+
+            return dictionary;
+        }
+
+        static void PutIntoFile(BitArray queue, Dictionary<char, byte> dict)
+        {
+            StreamWriter sw = new StreamWriter(@"D:\Studia\Zajęcia\Algorytmy i struktury danych\ćw\ASD\Projekt2 - kompresor\Kompresor\HoffmanCompresor\compessedFile.txt");
+
+            foreach (var item in dict)
+            {
+                sw.WriteLine(item);
+            }
+
+            foreach (bool item in queue)
+            {
+                char grzyb;
+                if (item == true)
+                    grzyb = '1';
+                else
+                    grzyb = '0';
+
+                sw.Write(grzyb);
+            }
+
+            Console.WriteLine("Done");
+            sw.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            string stringTocompress = "";
+            string input = ReadFromFileToString(stringTocompress);
+
+            Tree tree = new Tree();
+
+            tree.Build(input);
+            Dictionary<char, byte> dict = MakeDictionary(input);
+            BitArray encoded = tree.Encode(input);
+            PutIntoFile(encoded, dict);
+        }
+    }
+}
